917d69a5ddfa4e5f97fc4accb087096e9c20e4a6
JeroDietz <73513239+JeroDietz@users.noreply.github.com>
2021-03-26 18:31:08 -0300

    hasta aca llegue, falta ejs y db

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.get('/', indexRouter);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.listen(3000, function(){
     console.log('Servidor corriendo en el servidor 3000');
