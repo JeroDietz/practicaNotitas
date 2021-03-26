5c6dbdb5ec475cb786147f37e39f7a80fcf9edb8
JeroDietz <73513239+JeroDietz@users.noreply.github.com>
2021-03-26 17:59:35 -0300

    MVC funcionando

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const indexRouter = require('./router/indexRouter');
+
+
+app.get('/', indexRouter);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
+
+app.listen(3000, function(){
+    console.log('Servidor corriendo en el servidor 3000');
+    console.log('http://localhost:3000');
+})
---
+++ src/router/indexRouter.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const indexController = require('../controller/indexController')
+
+router.get('/', indexController.home);
+
+module.exports = router;
